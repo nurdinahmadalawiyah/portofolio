cb3ebee23d6deeebad418bba3c14191511729455
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-10-29 09:40:52 +0700

    complete work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,6 +6,12 @@ import { Card, CardBody } from "@nextui-org/card";
 import { siteConfig } from "@/config/site";
 import { useEffect, useState } from "react";
 import { Avatar } from "@nextui-org/avatar";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineContent from "@mui/lab/TimelineContent";
 
 export default function WorkPage() {
   const [tabSize, setTabSize] = useState<"lg" | "sm" | "md" | undefined>("lg");
@@ -49,7 +55,7 @@ export default function WorkPage() {
             <Tab key={item.company} title={item.company}>
               <Card>
                 <CardBody>
-                  <div className="flex gap-6 justify-center">
+                  <div className="flex gap-6 justify-left">
                     <Avatar size="lg" radius="sm" src={item.image} />
                     <div className="flex flex-col gap-0">
                       <h1 className="text-large font-medium">{item.company}</h1>
@@ -58,6 +64,33 @@ export default function WorkPage() {
                       </p>
                     </div>
                   </div>
+                  <Timeline
+                    sx={{
+                      [`& .${timelineItemClasses.root}:before`]: {
+                        flex: 0,
+                        padding: 0.45,
+                      },
+                    }}
+                  >
+                    {item.experience.map((experience, index) => (
+                      <TimelineItem key={item.company} >
+                        <TimelineSeparator>
+                          <TimelineDot sx={{ bgcolor: '#2CE7F1' }} />
+                          {index < item.experience.length - 1 && <TimelineConnector sx={{ bgcolor: '#2CE7F1' }} />}
+                        </TimelineSeparator>
+                        <TimelineContent>
+                          {experience.position}
+                          <p className="text-small text-foreground/80">
+                            {experience.type}
+                          </p>
+                          <p className="text-small text-foreground/80">
+                            {experience.date}
+                            {experience.duration && ` ● ${experience.duration}`}
+                          </p>
+                        </TimelineContent>
+                      </TimelineItem>
+                    ))}
+                  </Timeline>
                 </CardBody>
               </Card>
             </Tab>
